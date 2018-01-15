3ef8849943542b1c1ec0c38cf7c877508ee2a881
MSchalken <M.M.R.Schalken@student.tue.nl>
2018-01-15 21:20:16 +0100

    Initial setup for a random player, still have to figure out how to access data from the abstract class properly.

---
+++ RandomPlayer.py
@@ -0,0 +1,7 @@
+import random
+
+from AbstractPlayer import Player
+
+class RandomPlayer(Player):
+    for range(self._gamestate['m'] if self._playernr == 1 else self._gamestate['n']):
+        self._gamestate['points'].append({'x': random.uniform(0,self._gamestate['width']), 'y': random.uniform(0,self._gamestate['height'])})
