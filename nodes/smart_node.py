a8be95eb6bd0f8edfdfe573d02e2af9e3d66c439
Yatoom <jmapvhoof@gmail.com>
2018-02-01 09:58:30 +0100

    Implementation of smart nodes and smart tree.

---
+++ nodes/smart_node.py
@@ -0,0 +1,272 @@
+class SmartNode:
+    def __init__(self, data):
+        """
+        A smart tree node with some extra functionality over standard nodes.
+        :param data: Data that is stored inside the node.
+        """
+        self.data = data
+        self._left = None
+        self._right = None
+        self._height = None
+        self.parent = None
+
+    def __repr__(self):
+        return f"Node({self.data}, left={self.left}, right={self.right})"
+
+    @property
+    def left(self) -> "SmartNode":
+        return self._left
+
+    @property
+    def right(self) -> "SmartNode":
+        return self._right
+
+    @property
+    def grandparent(self):
+        if self.parent is None or self.parent.parent is None:
+            return None
+        return self.parent.parent
+
+    def get_key(self, **kwargs):
+        return self.data
+
+    def get_value(self, **kwargs):
+        return self.data
+
+    def get_label(self, **kwargs):
+        return f"{self.get_key(**kwargs)}({self.height})"
+
+    @left.setter
+    def left(self, node):
+
+        if node is not None:
+            # Avoid loops when rotating
+            self._avoid_loops(node)
+
+            # Tell the child who its new parent is
+            node.parent = self
+
+        self._left = node
+
+    @right.setter
+    def right(self, node):
+
+        if node is not None:
+            # Avoid loops when rotating
+            self._avoid_loops(node)
+
+            # Tell the child who its new parent is
+            node.parent = self
+
+        self._right = node
+
+    def _avoid_loops(self, node):
+
+        # Remove the pointer to this node from the parent
+        if node is self.parent and self.is_left_child():
+            self.parent.left = None
+        elif node is self.parent and self.is_right_child():
+            self.parent.right = None
+
+        # Remove the parent pointer from this node
+        self.parent = None
+
+    @property
+    def height(self):
+        if self._height is None:
+            self._height = self.calculate_height()
+        return self._height
+
+    @property
+    def balance(self):
+        left_height = self.left.height if self.left is not None else 0
+        right_height = self.right.height if self.right is not None else 0
+        return left_height - right_height
+
+    def calculate_height(self):
+        """
+        Recursively calculate height of this node.
+        Height calculated for each node will be stored, so calculations need to be done only once.
+        :return: (int) Height of the current node
+        """
+        left_height = self.left.height if self.left is not None else 0
+        right_height = self.right.height if self.right is not None else 0
+        height = 1 + max(left_height, right_height)
+        return height
+
+    def update_heights(self, propagate=True):
+        """
+        Recalculate the height of the node and optionally propagate new height calculations to ancestors.
+
+        :param propagate: (bool) Propagate to ancestors
+        """
+
+        # Calculate height
+        height = self.calculate_height()
+
+        # Set new height
+        self._height = height
+
+        # Update parent
+        if propagate and self.parent is not None:
+            self.parent.update_heights()
+
+    def is_left_child(self):
+        """
+        Determines whether this node is a left child.
+        :return: (bool) True if this node is a left child, False otherwise
+        """
+        if self.parent is None:
+            return False
+        return self.parent.left == self
+
+    def is_right_child(self):
+        """
+        Determines whether this node is a right child.
+        :return: (bool) True if this node is a right child, False otherwise
+        """
+        if self.parent is None:
+            return False
+        return self.parent.right == self
+
+    def is_leaf(self):
+        """
+        Determines whether this node is a leaf.
+        :return: (bool) True if this node is a leaf, False otherwise
+        """
+        return self.left is None and self.right is None
+
+    def minimum(self):
+        """
+        Determines the node with the smallest key in the subtree rooted by this node.
+        :return: (SmartNode) Node with the smallest key
+        """
+        current = self
+        while current.left is not None:
+            current = current.left
+        return current
+
+    def maximum(self):
+        """
+        Determines the node with the largest key in the subtree rooted by this node.
+        :return: (SmartNode) Node with the largest key
+        """
+        current = self
+        while current.right is not None:
+            current = current.right
+        return current
+
+    @property
+    def successor(self):
+        """
+        Returns the node with the smallest key larger than this node's key, or None
+        if this node has the largest key in the tree.
+        """
+
+        # If the node has a right sub tree, take the minimum
+        if self.right is not None:
+            return self.right.minimum()
+
+        # Walk up to the left until we are no longer a right child
+        current = self
+        while current.is_right_child():
+            current = current.parent
+
+        # Check there is a right branch
+        if current.parent is None or current.parent.right is None:
+            return None
+
+        # Step over to the right branch, and take the minimum
+        return current.parent.right.minimum()
+
+    @property
+    def predecessor(self):
+        """
+        Returns the node with the largest key smaller than this node's key, or None
+        if this node has the smallest key in the tree.
+        """
+
+        # If the node has a left sub tree, take the maximum
+        if self.left is not None:
+            return self.left.maximum()
+
+        # Walk up to the right until we are no longer a left child
+        current = self
+        while current.is_left_child():
+            current = current.parent
+
+        # Check there is a left branch
+        if current.parent is None or current.parent.left is None:
+            return None
+
+        # Step over to the left branch, and take the maximum
+        return current.parent.left.maximum()
+
+    def replace(self, replacement, root):
+        """
+        Replace the node by a replacement tree.
+        Requires the current node to be a leaf.
+
+        :param replacement: (SmartNode) The root node of the replacement sub tree
+        :param root: (SmartNode) The root of the tree
+        :return: (SmartNode) The root of the updated tree
+        """
+
+        if not self.is_leaf():
+            raise ValueError("Node to be replaced is not a leaf")
+
+        # Give the parent of the node to the replacement
+        replacement.parent = self.parent
+
+        # If node is left child, replace it by giving the parent a new left node
+        if self.is_left_child():
+            self.parent.left = replacement
+
+        # If node is right child, replace it by giving the parent a new right node
+        elif self.is_right_child():
+            self.parent.right = replacement
+
+        # Otherwise, replace the root
+        else:
+            root = replacement
+
+        # Update all the heights
+        replacement.update_heights()
+
+        # Return the new tree. No need to return the replacement, because the
+        # reference remains the same.
+        return root
+
+    def visualize(self, depth=0):
+        """
+        Visualize the node and its descendants.
+
+        :param depth: (int) Used by the recursive formula, should be left at the default value
+        :return: (str) String representation of the visualization
+        """
+        ret = ""
+
+        # Print right branch
+        if self.right is not None:
+            ret += self.right.visualize(depth + 1)
+
+        # Print own value
+        ret += "\n" + ("    " * depth) + str(self.get_label())
+
+        # Print left branch
+        if self.left is not None:
+            ret += self.left.visualize(depth=depth + 1)
+
+        return ret
+
+
+if __name__ == '__main__':
+    node = SmartNode(5)
+    node.left = SmartNode(3)
+    node.right = SmartNode(7)
+    node.left.left = SmartNode(2)
+    node.left.right = SmartNode(4)
+    node.right.left = SmartNode(6)
+    node.right.right = SmartNode(8)
+
+    print(node.visualize())
---
+++ nodes/smart_tree.py
@@ -0,0 +1,216 @@
+from nodes.smart_node import SmartNode
+import time
+
+
+class SmartTree:
+    def __init__(self):
+        """
+        Self-balancing Binary Search Tree.
+        """
+        self.root = None
+
+    @staticmethod
+    def find(root: SmartNode, key: int, **kwargs):
+
+        node = root
+        while node is not None:
+            if key == node.get_key(**kwargs):
+                break
+            elif key < node.get_key(**kwargs):
+                node = node.left
+            else:
+                node = node.right
+
+        # Return node, None if not found
+        return node
+
+    @staticmethod
+    def insert(root: SmartNode, node: SmartNode, **kwargs):
+
+        # Get keys once
+        node_key = node.get_key(**kwargs) if node is not None else None
+        root_key = root.get_key(**kwargs) if root is not None else None
+
+        # Binary Search Tree insert
+        if root is None:
+            return node
+        elif node_key < root_key:
+            root.left = SmartTree.insert(root.left, node, **kwargs)
+        else:
+            root.right = SmartTree.insert(root.right, node, **kwargs)
+
+        # Update the height of the ancestor node
+        root.update_heights(propagate=False)
+
+        # If the node is unbalanced, then try out the 4 cases
+        balance = root.balance
+        # root = SmartTree.balance(root)
+
+        # Case 1 - Left Left
+        if balance > 1 and node_key < root.left.get_key(**kwargs):
+            return SmartTree.rotate_right(root)
+
+        # Case 2 - Right Right
+        if balance < -1 and node_key > root.right.get_key(**kwargs):
+            return SmartTree.rotate_left(root)
+
+        # Case 3 - Left Right
+        if balance > 1 and node_key > root.left.get_key(**kwargs):
+            root.left = SmartTree.rotate_left(root.left)
+            return SmartTree.rotate_right(root)
+
+        # Case 4 - Right Left
+        if balance < -1 and node_key < root.right.get_key(**kwargs):
+            root.right = SmartTree.rotate_right(root.right)
+            return SmartTree.rotate_left(root)
+
+        return root
+
+    @staticmethod
+    def delete(root: SmartNode, key: int, **kwargs):
+
+        if root is None:
+            return root
+
+        elif key < root.get_key():
+            root.left = SmartTree.delete(root.left, key)
+
+        elif key > root.get_key():
+            root.right = SmartTree.delete(root.right, key)
+
+        else:
+            if root.left is None:
+                return root.right
+
+            elif root.right is None:
+                return root.left
+
+            temp = root.right.minimum()
+            root.data = temp.data
+            root.right = SmartTree.delete(root.right, temp.value.get_key(**kwargs))
+
+        # If the tree has only one node, simply return it
+        if root is None:
+            return root
+
+        # Update the height of the ancestor node
+        root.update_heights(propagate=False)
+
+        # Balance the tree
+        root = SmartTree.balance(root)
+
+        return root
+
+    @staticmethod
+    def balance(node):
+        """
+        Make the three balanced if it is unbalanced.
+        :param node: (Node) The root node of the tree to balance
+        :return: (Node) The new root of the sub tree
+        """
+
+        # If the node is unbalanced, then try out the 4 cases
+
+        # Case 1 - Left Left
+        if node.balance > 1 and node.left.balance >= 0:
+            return SmartTree.rotate_right(node)
+
+        # Case 2 - Right Right
+        if node.balance < -1 and node.right.balance <= 0:
+            return SmartTree.rotate_left(node)
+
+        # Case 3 - Left Right
+        if node.balance > 1 and node.left.balance < 0:
+            node.left = SmartTree.rotate_left(node.left)
+            return SmartTree.rotate_right(node)
+
+        # Case 4 - Right Left
+        if node.balance < -1 and node.right.balance > 0:
+            node.right = SmartTree.rotate_right(node.right)
+            return SmartTree.rotate_left(node)
+
+        return node
+
+    @staticmethod
+    def rotate_left(z):
+        """
+        Rotate tree to the left.
+
+        # T1, T2, T3 and T4 are subtrees.
+        #     z                               y
+        #    / \                            /  \
+        #   T1   y     Left Rotate(z)      z     x
+        #       / \   - - - - - - - ->   / \    / \
+        #      T2  x                    T1  T2 T3  T4
+        #         / \
+        #       T3  T4
+
+        :param z: (Node) The root of the sub tree
+        :return: (Node) The new root of the sub tree
+        """
+
+        y = z.right
+        T2 = y.left
+
+        # Perform rotation
+        y.left = z
+        z.right = T2
+
+        # Update heights (z has to be updated first, because it is a child of y)
+        z.update_heights(propagate=False)
+        y.update_heights(propagate=False)
+
+        # Return the new root
+        return y
+
+    @staticmethod
+    def rotate_right(z):
+        """
+        Rotate tree to the right.
+
+        # T1, T2, T3 and T4 are subtrees.
+        #          z                                      y
+        #         / \                                   /   \
+        #        y   T4      Right Rotate (z)          x      z
+        #       / \          - - - - - - - - ->      /  \    /  \
+        #      x   T3                               T1  T2  T3  T4
+        #     / \
+        #   T1   T2
+
+        :param z: (Node) The root of the sub tree
+        :return: (Node) The new root of the sub tree
+        """
+
+        y = z.left
+        T3 = y.right
+
+        # Perform rotation
+        y.right = z
+        z.left = T3
+
+        # Update heights (z has to be updated first, because it is a child of y)
+        z.update_heights(propagate=False)
+        y.update_heights(propagate=False)
+
+        # Return the new root
+        return y
+
+
+if __name__ == '__main__':
+    start = time.time()
+    tree = SmartTree()
+
+    nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    # nodes = range(1, 100000)
+    for i in nodes:
+        sn = SmartNode(i)
+        tree.root = tree.insert(tree.root, sn)
+        # print("-" * 20)
+        # print(tree.root.visualize())
+
+    # tree.root = tree.balance(tree.root)
+    tree.root = tree.delete(tree.root, key=3)
+
+    end = time.time()
+    print("Time", end - start)
+    print(tree.root.visualize())
