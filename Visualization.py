e174961f7c0eb3a82697fa39a45c4e1ce3a610f8
SAN-FRANSISCO <roel.hospel@gmail.com>
2018-02-20 22:15:13 +0100

    Fixed Visualization

--- Visualization.py
+++ Visualization.py
@@ -20,10 +20,10 @@ class Visualization(GameState):
     colour_polygon_unknown = svgwrite.rgb(160, 160, 160, '%')
     colour_point_unknown = svgwrite.rgb(255, 255, 255, '%')
 
-    strokewidth_bounding_box = '5'
-    strokewidth_edges = '5'
+    strokewidth_bounding_box = '2'
+    strokewidth_edges = '1'
 
-    size_point = '1'
+    size_point = '5'
 
     def create_visualization(self, name):
         # Construct a Voronoi for the points in the GameState
@@ -35,12 +35,12 @@ class Visualization(GameState):
             name = name + '.svg'
 
         # Initiate SVG image
-        play_board = svgwrite.Drawing(name, profile='tiny')
+        play_board = svgwrite.Drawing(name, size=('250', '250'), profile='tiny')
 
         # For each vonoroi polygon, create a polygon
         edges_seen = []
         for edge in voronoi.edges:
-            play_board.add(play_board.line((edge.origin.position.x, edge.origin.position.y), (edge.twin.origin.position.x, edge.twin.origin.position.y),
+            play_board.add(play_board.line((edge.origin.position.x * 10, edge.origin.position.y * 10), (edge.twin.origin.position.x * 10, edge.twin.origin.position.y * 10),
                                            stroke=self.colour_edges,
                                            stroke_width=self.strokewidth_edges))
             # Define the colour of the face
@@ -52,20 +52,20 @@ class Visualization(GameState):
                 colour_point = self.colour_point_unknown
 
             # Add Point to SVG
-            play_board.add(play_board.circle([edge.incident_point.x, edge.incident_point.y],
+            play_board.add(play_board.circle([edge.incident_point.x * 10, edge.incident_point.y * 10],
                                              fill=colour_point, r='1'))
 
         # Add bounding Box to SVG
-        play_board.add(play_board.line((0, 0), (self.state.width, 0),
+        play_board.add(play_board.line((0, 0), (self.state.width * 10, 0),
                                        stroke=self.colour_bounding_box,
                                        stroke_width=self.strokewidth_bounding_box))
-        play_board.add(play_board.line((0, self.state.height), (self.state.width, self.state.height),
+        play_board.add(play_board.line((0, self.state.height * 10), (self.state.width * 10, self.state.height * 10),
                                        stroke=self.colour_bounding_box,
                                        stroke_width=self.strokewidth_bounding_box))
-        play_board.add(play_board.line((0, 0), (0, self.state.height),
+        play_board.add(play_board.line((0, 0), (0, self.state.height * 10),
                                        stroke=self.colour_bounding_box,
                                        stroke_width=self.strokewidth_bounding_box))
-        play_board.add(play_board.line((self.state.width, 0), (self.state.width, self.state.height),
+        play_board.add(play_board.line((self.state.width * 10, 0), (self.state.width * 10, self.state.height * 10),
                                        stroke=self.colour_bounding_box,
                                        stroke_width=self.strokewidth_bounding_box))
 
