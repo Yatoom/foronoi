bf3c0a773aea9890306fa114856c4ac1c167b78d
Yatoom <jmapvhoof@gmail.com>
2018-03-28 00:47:47 +0200

    Refactored unit tests.

--- tests/unit.py
+++ tests/unit.py
@@ -4,38 +4,87 @@ from graph.bounding_box import BoundingBox
 from graph.point import Point
 
 
-def test_grid():
-    # Testing square grid
-    points = []
-    for i in range(25, 0, -5):
-        for j in range(0, 25, 5):
-            points.append(Point(j, i))
-    v = Algorithm(BoundingBox(-5, 30, -5, 30))
-    v.create_diagram(points=points, vis_steps=False, vis_result=False, verbose=False, vis_tree=False)
-    sizes = [56.25, 37.5, 37.5, 37.5, 93.75, 37.5, 25.0, 25.0, 25.0, 62.5, 37.5, 25.0, 25.0, 25.0, 62.5, 37.5, 25.0,
-             25.0, 25.0, 62.5, 93.75, 62.5, 62.5, 62.5, 156.25]
+# -----------------
+# Helper functions
+# -----------------
+
+def _triangle(x, y):
+    return Polygon([
+        Point(0, y),
+        Point(x, y),
+        Point(x / 2, 0)
+    ])
+
+
+def _execute(polygon, points, sizes):
+    v = Algorithm(polygon)
+    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
     calculated = [p.cell_size(2) for p in v.points]
     assert (sizes == calculated)
 
 
-def test_diamond():
-    # Testing diamond
+# -----------
+# Test cases
+# -----------
 
+def test_single_point_triangle():
+    # Polygon
+    polygon = _triangle(100, 100)
+
+    # Points
+    points = [
+        Point(50, 50),
+    ]
+
+    # Expected sizes
+    sizes = [5000]
+
+    # Execute test
+    _execute(polygon, points, sizes)
+
+
+def test_grid():
+    # Polygon
+    polygon = BoundingBox(-5, 30, -5, 30)
+
+    # Points
+    points = []
+    for i in range(25, 0, -5):
+        for j in range(0, 25, 5):
+            points.append(Point(j, i))
+
+    # Expected sizes
+    sizes = [56.25, 37.5, 37.5, 37.5, 93.75, 37.5, 25.0, 25.0, 25.0, 62.5, 37.5, 25.0, 25.0, 25.0, 62.5, 37.5, 25.0,
+             25.0, 25.0, 62.5, 93.75, 62.5, 62.5, 62.5, 156.25]
+
+    # Execute test
+    _execute(polygon, points, sizes)
+
+
+def test_diamond():
+    # Polygon
+    polygon = BoundingBox(0, 10, 0, 10)
+
+    # Points
     points = [
         Point(5, 7.5),
         Point(2.5, 5),
         Point(7.5, 5),
         Point(5, 2.5),
     ]
-    v = Algorithm(BoundingBox(0, 10, 0, 10))
-    v.create_diagram(points=points, vis_steps=False, vis_result=False, verbose=False)
+
+    # Expected sizes
     sizes = [25.0, 25.0, 25.0, 25.0, ]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_martijn():
+    # Polygon
+    polygon = BoundingBox(-1, 26, -1, 26)
 
+    # Points
     points = [
         Point(2.241, 3.594),
         Point(3.568, 3.968),
@@ -43,15 +92,18 @@ def test_martijn():
         Point(2.925, 18.298),
     ]
 
-    v = Algorithm(BoundingBox(-1, 26, -1, 26))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
+    # Expected sizes
     sizes = [42.86, 209.3, 380.84, 96.0]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_roel():
+    # Polygon
+    polygon = BoundingBox(0, 30, 0, 30)
 
+    # Points
     points = [
         Point(8.333, 8.333),
         Point(8.333, 26),
@@ -59,15 +111,18 @@ def test_roel():
         Point(26, 17.667)
     ]
 
-    v = Algorithm(BoundingBox(0, 30, 0, 30))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
+    # Expected sizes
     sizes = [214.58, 229.05, 221.56, 234.8]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_desmos():
+    # Polygon
+    polygon = BoundingBox(0, 25, 0, 25)
 
+    # Points
     points = [
         Point(4.6, 11.44),
         Point(10, 15.44),
@@ -83,30 +138,36 @@ def test_desmos():
         Point(7.8, 8.4),
     ]
 
-    v = Algorithm(BoundingBox(0, 25, 0, 25))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
+    # Expected sizes
     sizes = [26.01, 202.48, 15.87, 95.95, 12.32, 108.89, 14.05, 57.78, 31.34, 12.08, 31.21, 17.03]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_rounding_errors():
+    # Polygon
+    polygon = BoundingBox(0, 25, 0, 25)
 
+    # Points
     points = [
         Point(10, 3),
         Point(13.9, 6.76),
         Point(12, 1.20),
     ]
 
-    v = Algorithm(BoundingBox(0, 25, 0, 25))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
+    # Expected sizes
     sizes = [128.59, 465.07, 31.34]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_corners():
+    # Polygon
+    polygon = BoundingBox(0, 10, 0, 10)
 
+    # Points
     points = [
         Point(0, 10),
         Point(10, 0),
@@ -114,34 +175,36 @@ def test_corners():
         Point(10, 10),
     ]
 
-    v = Algorithm(BoundingBox(0, 10, 0, 10))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
+    # Expected sizes
     sizes = [25.0, 25.0, 25.0, 25.0]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_horizontal():
+    # Polygon
+    polygon = BoundingBox(0, 8, 0, 10)
 
+    # Points
     points = [
         Point(2, 2.5),
         Point(4, 2.5),
         Point(6, 2.5),
     ]
 
-    v = Algorithm(BoundingBox(0, 8, 0, 10))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [30.0, 20.0, 30.0]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_left_arc():
+    # Polygon
+    polygon = BoundingBox(0, 25, 0, 25)
 
-    from algorithm import Algorithm
-    from graph import Point, BoundingBox
-
+    # Points
     points = [
         Point(3.125, 3.125),
         Point(9.375, 3.125),
@@ -150,16 +213,18 @@ def test_left_arc():
         Point(3.125, 9.375),
     ]
 
-    v = Algorithm(BoundingBox(0, 25, 0, 25))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [39.06, 58.59, 117.19, 156.25, 253.91]
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_multi_diamond():
+    # Polygon
+    polygon = BoundingBox(-1, 26, -1, 26)
 
+    # Points
     points = [
         Point(3.125, 3.125),
         Point(9.375, 3.125),
@@ -194,75 +259,48 @@ def test_multi_diamond():
         Point(25.0, 18.75)
     ]
 
-    v = Algorithm(BoundingBox(-1, 26, -1, 26))
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [47.68, 35.55, 35.55, 47.68, 27.04, 19.53, 19.53, 35.55, 19.53, 19.53, 19.53, 27.04, 30.66, 19.53, 19.53,
              30.66, 19.53, 19.53, 19.53, 19.53, 19.53, 19.53, 19.53, 19.53, 19.53, 16.52, 16.52, 16.52, 16.02, 16.52,
              17.02]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_triangle_from_left():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
-
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
+    # Points
     points = [Point(13, 93), Point(20, 89), Point(33, 69)]
 
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [218.59, 629.68, 4151.73]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_triangle_from_right():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
+    # Points
+    points = [Point(100 - 13, 93), Point(100 - 20, 89), Point(100 - 33, 69)]
 
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
-    points = [Point(x - 13, 93), Point(x - 20, 89), Point(x - 33, 69)]
-
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [218.59, 629.68, 4151.73]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_lines_outside_triangle():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
-
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
+    # Points
     points = [
         Point(57, 75),
         Point(35, 85),
@@ -270,55 +308,36 @@ def test_lines_outside_triangle():
         Point(81, 87),
     ]
 
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [2590.11, 2590.11, 147.0, 673.12]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_another_line_outside_triangle():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
-
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
+    # Points
     points = [
         Point(54, 90),
         Point(5, 95),
         Point(16, 85),
     ]
 
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [3490.08, 1373.73, 1373.73]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_max_distance():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
-
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
-
+    # Points
     points = [
         Point(45, 13),
         Point(57, 71),
@@ -326,28 +345,18 @@ def test_max_distance():
         Point(61, 81),
     ]
 
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [899.09, 1299.22, 1629.42, 1172.27]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
 
 
 def test_calc_cell_sizes():
+    # Polygon
+    polygon = _triangle(100, 100)
 
-    x = 100
-    y = 100
-
-    polygon_points = [
-        Point(0, y),
-        Point(x, y),
-        Point(x / 2, 0)
-    ]
-
-    polygon = Polygon(polygon_points)
-
+    # Points
     points = [
         Point(45, 13),
         Point(43, 85),
@@ -356,10 +365,8 @@ def test_calc_cell_sizes():
         Point(27, 90),
     ]
 
-    v = Algorithm(polygon)
-    v.create_diagram(points=points, vis_steps=False, verbose=False, vis_result=False)
-
+    # Expected sizes
     sizes = [1161.4, 1958.77, 1128.36, 341.33, 410.13]
 
-    calculated = [p.cell_size(2) for p in v.points]
-    assert (sizes == calculated)
+    # Execute test
+    _execute(polygon, points, sizes)
