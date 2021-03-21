0be57ca617919f6bf1c7d468b6d5680064a93f47
Yatoom <jmapvhoof@gmail.com>
2021-03-22 00:32:37 +0100

    Updated setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
   name='voronoi',
-  packages=['voronoi', 'voronoi.visualization', 'voronoi.tree', 'voronoi.nodes', "voronoi.graph", "voronoi.events", "voronoi.tests"],
+  packages=[
+    'voronoi',
+    'voronoi.visualization',
+    'voronoi.contrib',
+    'voronoi.observers',
+    'voronoi.tree',
+    'voronoi.nodes',
+    "voronoi.graph",
+    "voronoi.events",
+    "voronoi.tests"
+  ],
   version='0.9',
   description="Fortune's algorithm for Voronoi diagrams with polygon bounding boxes.",
   author='Jeroen van Hoof',
@@ -10,5 +20,7 @@ setup(
   download_url='',
   keywords=['voronoi', 'polygon', 'fortune', 'algorithm'],
   classifiers=[],
-  install_requires=find_packages()
+  install_requires=[
+    "numpy", "matplotlib", "graphviz"
+  ]
 )
